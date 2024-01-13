76e4b85158c5d1014454c49b0e1082fd821c49e4
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2024-01-13 17:23:39 +0600

    added styles and classNames

--- src/components/AddIngredient/AddIngredient.tsx
+++ src/components/AddIngredient/AddIngredient.tsx
@@ -1,59 +1,57 @@
-import React from 'react';
-import './AddIngredient.css';
+import React from "react";
+import "./AddIngredient.css";
 
 interface MenuItem {
-	name: string;
-	price: number;
-	image: string;
+  name: string;
+  price: number;
+  image: string;
 }
 
 interface AddIngredientProps {
-	menuItems: MenuItem[];
-	onAddIng: (name: string) => void;
-	onRemoveItem: (name: string) => void;
+  menuItems: MenuItem[];
+  onAddIng: (name: string) => void;
+  onRemoveItem: (name: string) => void;
 }
 
 const AddIngredient: React.FC<AddIngredientProps> = ({
-	menuItems,
-	onAddIng,
-	onRemoveItem,
+  menuItems,
+  onAddIng,
+  onRemoveItem,
 }) => {
-	return (
-		<div className='AddIngredient'>
-			<ul>
-				{menuItems.map((item, index) => (
-					<li key={index} className='AddIngredient-list'>
-						<div>
-							<img
-								src={`${item.image}`}
-								alt={item.name}
-								className='AddIngredient-images'
-							/>
-						</div>
-						<div>
-							<p>
-								{item.name} - {item.price}
-							</p>
-							<button
-								onClick={() => onAddIng(item.name)}
-								className='
-							AddIngredient-Button'
-							>
-								Add
-							</button>
-							<button
-								onClick={() => onRemoveItem(item.name)}
-								className='
-							AddIngredient-Button'
-							>
-								Remove
-							</button>
-						</div>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="AddIngredient">
+      <ul className="AddIngredient-list">
+        {menuItems.map((item, index) => (
+          <li key={index} className="AddIngredient-list">
+            <div>
+              <img
+                src={`${item.image}`}
+                alt={item.name}
+                className="AddIngredient-images"
+              />
+            </div>
+            <div>
+              <p>
+                {item.name} - {item.price}
+              </p>
+              <button
+                onClick={() => onAddIng(item.name)}
+                className="AddIngredient-Button"
+              >
+                Add
+              </button>
+              <button
+                onClick={() => onRemoveItem(item.name)}
+                className="AddIngredient-Button"
+              >
+                Remove
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AddIngredient;
