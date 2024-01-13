c761dc09df4431b5b69de76a33b7273ac0a475a1
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2024-01-13 15:06:04 +0600

    Created and added AddIgredient component and which add and delete ingredients

---
+++ src/components/AddIngredient/AddIngredient.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './AddIngredient.css';
+
+interface MenuItem {
+	name: string;
+	price: number;
+	image: string;
+}
+
+interface AddIngredientProps {
+	menuItems: MenuItem[];
+	onAddIng: (name: string) => void;
+	onRemoveItem: (name: string) => void;
+}
+
+const AddIngredient: React.FC<AddIngredientProps> = ({
+	menuItems,
+	onAddIng,
+	onRemoveItem,
+}) => {
+	return (
+		<div className='AddIngredient'>
+			<ul>
+				{menuItems.map((item, index) => (
+					<li key={index} className='AddIngredient-list'>
+						<div>
+							<img
+								src={`${item.image}`}
+								alt={item.name}
+								className='AddIngredient-images'
+							/>
+						</div>
+						<div>
+							<p>
+								{item.name} - ${item.price}
+							</p>
+							<button
+								onClick={() => onAddIng(item.name)}
+								className='
+							AddIngredient-Button'
+							>
+								Add
+							</button>
+							<button
+								onClick={() => onRemoveItem(item.name)}
+								className='
+							AddIngredient-Button'
+							>
+								Remove
+							</button>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default AddIngredient;
